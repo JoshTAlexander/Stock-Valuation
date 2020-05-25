778f8e918b0c604873cd7be9d6cda8fd19b62599
JoshTAlexander <63522262+JoshTAlexander@users.noreply.github.com>
2020-05-25 23:37:00 +0100

    Add files via upload

--- Stock Valuation/eps.py
+++ Stock Valuation/eps.py
@@ -21,81 +21,92 @@ browser.get(url)
 accept_button_xpath = "//div/button[contains(text(), 'Accept')]"
 accept_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, accept_button_xpath))).click()
 
-#get compnay name
-company_xpath = "//h1[contains(text(), {})]".format(symbol.upper())
-company = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, company_xpath))).text
-print(company)
+try:
+    #get compnay name
+    company_xpath = "//h1[contains(text(), {})]".format(symbol.upper())
+    company = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, company_xpath))).text
+    print(company)
 
-#get price
-price_xpath = "//html/body/div[2]/div/main/div/div[4]/section/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/span/span[2]"
-price = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, price_xpath))).text
-print("Price: " + str(price))
-price = float(price.replace("$",""))
+    #get price
+    price_xpath = "//html/body/div[2]/div/main/div/div[4]/section/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/span/span[2]"
+    price = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, price_xpath))).text
+    print("Price: " + str(price))
+    price = float(price.replace("$",""))
 
-#get elements and add to a list
-def get_elements(xpath):
-    elements = browser.find_elements_by_xpath(xpath) # find the elements
-    text = []
-    for e in elements:
-        text.append(e.text)
-    return text
+    #get elements and add to a list
+    def get_elements(xpath):
+        elements = browser.find_elements_by_xpath(xpath) # find the elements
+        text = []
+        for e in elements:
+            text.append(e.text)
+        return text
 
-#get earnings per share
-earningspershare = get_elements("//html/body/div[2]/div/main/div/div[5]/div[2]/div/div[1]/div/div[4]/div[1]/div/div/table/tbody/tr[1]/td[1]")
-earningspershare=float(earningspershare[0])
+    #get earnings per share
+    earningspershare = get_elements("//html/body/div[2]/div/main/div/div[5]/div[2]/div/div[1]/div/div[4]/div[1]/div/div/table/tbody/tr[1]/td[1]")
+    earningspershare=float(earningspershare[0])
 
-#get price per earnings
-pe_button_xpath = "//html/body/div[2]/div/main/div/div[4]/div/section/div/div[3]/ul/li[6]/a/span"
-pe_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, pe_button_xpath))).click()
-time.sleep(10)
-pe = get_elements("//main/div/div[5]/div[2]/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[2]/td")
-pe=float(pe[0])
+    #get price per earnings
+    pe_button_xpath = "//html/body/div[2]/div/main/div/div[4]/div/section/div/div[3]/ul/li[6]/a/span"
+    pe_button = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, pe_button_xpath))).click()
+    time.sleep(10)
+    pe = get_elements("//main/div/div[5]/div[2]/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[1]/td")
+    pe=float(pe[0])
 
-#get peg ratio
-pegratio = get_elements("//main/div/div[5]/div[2]/div/div[1]/div/div[1]/div[4]/div[2]/div[2]/table/tbody/tr/td")
-pegratio=float(pegratio[0])
+    #get peg ratio
+    pegratio = get_elements("//main/div/div[5]/div[2]/div/div[1]/div/div[1]/div[4]/div[2]/div[2]/table/tbody/tr/td")
+    pegratio=float(pegratio[0])
 
-#get pe industry average
-peindurl = "https://www.gurufocus.com/term/pettm/{}/PE-Ratiottm"
-url = peindurl.format(symbol)
-browser.get(url)
-peind = get_elements("//html/body/div[2]/div[2]/div/div/div/div/div/div/div/div")
-peind = peind[1]
-m = re.search('Industry Median: (.+?) vs', peind)
-if m:
-    found = m.group(1)
-peind = found
-peind = float(peind)
+    #get pe industry average
+    peindurl = "https://www.gurufocus.com/term/pettm/{}/PE-Ratiottm"
+    url = peindurl.format(symbol)
+    browser.get(url)
+    peind = get_elements("//html/body/div[2]/div[2]/div/div/div/div/div/div/div/div")
+    peind = peind[1]
+    try:
+        m = re.search('Industry Median: (.+?) vs', peind)
+        if m:
+            found = m.group(1)
+        peind = found
+        peind = float(peind)
+    except:
+        print("")
 
-#print results
-value = earningspershare*pe
-value = "{:.2f}".format(value)
-print("Forecast intrinsic Value: $" + str(value))
-#over or under
-if (price>float(value)):
-    print("(Over valued)")
-if (price<float(value)):
-    print("(Under valued)")
-if (price==float(value)):
-    print("(Hold)")
+    #print results
+    value = earningspershare*pe
+    value = "{:.2f}".format(value)
+    print("Intrinsic Value: $" + str(value))
+    #over or under
+    if (price>float(value)):
+        print("(Over valued)")
+    if (price<float(value)):
+        print("(Under valued)")
+    if (price==float(value)):
+        print("(Hold)")
 
-#peg ratio comparison
-print("PEG Ratio = " + str(pegratio))
-if (pegratio<1):
-    print("(Under Valued)")
-if (pegratio>1):
-    print("(Over Valued)")
-if (pegratio==0.0):
-    print("(Hold)")
+    #peg ratio comparison
+    print("PEG Ratio = " + str(pegratio))
+    if (pegratio<1):
+        print("(Growth opportunity?)")
+    if (pegratio>1):
+        print("(Not a growth opportunity?)")
+    if (pegratio==1):
+        print("(Hold)")
 
-#pe comparison
-print("PE Ratio: "+str(pe))
-print("Industry Average PE Ratio: " + str(peind))
-if (pe>peind):
-    print("(Over Valued)")
-if (pe<peind):
-    print("(Under Vauled)")
-if (pe==peind):
-    print("(Hold)")
+    #pe comparison
+    print("PE Ratio: "+str(pe))
+    try:
+        if (type(peind)==float):
+            print("Industry Average PE Ratio: " + str(peind))
+        if (pe>peind):
+            print("(Not a growth opportunity?)")
+        if (pe<peind):
+            print("(Growth opportuntuy?)")
+        if (pe==peind):
+            print("(Hold)")
+    except:
+        print("")
+
+except:
+    print("Stock not listed on NASDAQ or does not pay dividends")
 
 browser.quit()
--- Stock Valuation/gordons.py
+++ Stock Valuation/gordons.py
@@ -17,61 +17,67 @@ url_form = "https://www.nasdaq.com/market-activity/stocks/{}/dividend-history"
 url = url_form.format(symbol)
 browser.get(url)
 
-#get compnay name
-company_xpath = "//h1[contains(text(), {})]".format(symbol.upper())
-company = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, company_xpath))).text
-print(company)
+try:
+    #get compnay name
+    company_xpath = "//h1[contains(text(), {})]".format(symbol.upper())
+    company = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, company_xpath))).text
+    print(company)
 
-#get price
-price_xpath = "//html/body/div[2]/div/main/div/div[3]/section/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/span/span[2]"
-price = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, price_xpath))).text
-print("Price: " + str(price))
-price = float(price.replace("$",""))
+    #get price
+    price_xpath = "//html/body/div[2]/div/main/div/div[3]/section/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/span/span[2]"
+    price = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, price_xpath))).text
+    print("Price: " + str(price))
+    price = float(price.replace("$",""))
 
-#get elements and add to a list
-def get_elements(xpath):
-    elements = browser.find_elements_by_xpath(xpath) # find the elements
-    text = []
-    for e in elements:
-        text.append(e.text)
-    return text
+    #get elements and add to a list
+    def get_elements(xpath):
+        elements = browser.find_elements_by_xpath(xpath) # find the elements
+        text = []
+        for e in elements:
+            text.append(e.text)
+        return text
 
-#get current div value
-currentdiv = get_elements("//html/body/div[2]/div/main/div/div[4]/div[1]/div/div[2]/ul/li[3]/span[2]/span")
-currentdiv = float(currentdiv[0].replace("$",""))
+    #get current div value
+    currentdiv = get_elements("//html/body/div[2]/div/main/div/div[4]/div[1]/div/div[2]/ul/li[3]/span[2]/span")
+    currentdiv = float(currentdiv[0].replace("$",""))
 
-#wacc
-waccurl = "https://www.gurufocus.com/term/wacc/{}/WACC/"
-url = waccurl.format(symbol)
-browser.get(url)
-wacc = get_elements("//html/body/div[2]/div[2]/div/div/div/div[2]/font[1]")
-wacc = wacc[0].replace(":","")
-wacc = (float(wacc.replace("% As of Today","")))/100
+    #wacc
+    waccurl = "https://www.gurufocus.com/term/wacc/{}/WACC/"
+    url = waccurl.format(symbol)
+    browser.get(url)
+    wacc = get_elements("//html/body/div[2]/div[2]/div/div/div/div[2]/font[1]")
+    wacc = wacc[0].replace(":","")
+    wacc = (float(wacc.replace("% As of Today","")))/100
 
-#get div growth rate
-divgrowthurl = "https://www.gurufocus.com/term/dividend_growth_3y/{}/3-Year-Dividend-Growth-Rate/"
-url = divgrowthurl.format(symbol)
-browser.get(url)
-divgrowth = get_elements("//html/body/div[2]/div[2]/div/div/div/div[2]/font[1]")
-m = re.search(': (.+?)%', divgrowth[0])
-if m:
-    found = m.group(1)
-divgrowth = found
-divgrowth = float(divgrowth)/100
+    #get div growth rate
+    divgrowthurl = "https://www.gurufocus.com/term/dividend_growth_3y/{}/3-Year-Dividend-Growth-Rate/"
+    url = divgrowthurl.format(symbol)
+    browser.get(url)
+    divgrowth = get_elements("//html/body/div[2]/div[2]/div/div/div/div[2]/font[1]")
+    m = re.search(': (.+?)%', divgrowth[0])
+    if m:
+        found = m.group(1)
+    divgrowth = found
+    divgrowth = float(divgrowth)/100
 
-#calculation
-intrinsicprice=((currentdiv*(1+divgrowth))/(wacc - divgrowth))
+    #calculation
+    intrinsicprice=((currentdiv*(1+divgrowth))/(wacc - divgrowth))
 
 
-#print results
-intrinsicprice="{:.2f}".format(intrinsicprice)
-print("Gordons Intrinsic Value: $" + str(intrinsicprice))
+    #print results
+    intrinsicprice="{:.2f}".format(intrinsicprice)
+    print("Gordons Intrinsic Value: $" + str(intrinsicprice))
 
-#over or under
-if (price>float(intrinsicprice)):
-    print ("(Over valued)")
-else:
-    print ("(Under valued)")
-print("If negative then div growth>wacc")
+    #over or under
+    if (price>float(intrinsicprice)):
+        print ("(Over valued)")
+    if (price<float(intrinsicprice)):
+        print ("(Under valued)")
+    if (price==float(intrinsicprice)):
+        print ("(Hold)")
+    print("If negative then div growth>wacc")
+
+except:
+    print("Data not found")
 
 browser.quit()
